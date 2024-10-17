8524c96f7ffce9ed5892250199d10b9cb28c55d6
ARJUN P <arjunpchr@gmail.com>
2024-10-18 02:13:47 +0530

    refactor: cors option modified with new frontend url

--- server/src/server.js
+++ server/src/server.js
@@ -17,7 +17,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://patient-health-dashboard-for-prior-authorization.vercel.app"],
+  origin: ["https://patient-health-dashboard-for-prior-authorization.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
